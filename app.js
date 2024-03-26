3514a77b946a62a704049cd3296bebcd84a82e72
Heandy27 <122308561+Heandy27@users.noreply.github.com>
2024-03-26 02:16:51 +0000

    [Creacion de app.js y test.js] [Testeo de monedas con funciones]

---
+++ app.js
@@ -0,0 +1,41 @@
+// Esta es mi función que suma dos números
+const sum = (a,b) => {
+    return a + b
+}
+
+
+let oneEuroIs = {
+    "JPY": 156.5, // japan yen
+    "USD": 1.07, // us dollar
+    "GBP": 0.87, // british pound
+}
+
+// Declaramos una función con el nombre exacto "formEuroToDollar"
+const fromEuroToDollar = function(valueInEuro) {
+    // Convertimos el valor a dólares
+    let valueInDollar = valueInEuro * oneEuroIs.USD;
+    // Retornamos el valor en dólares
+    return valueInDollar;
+}
+
+const fromDollarToYen = function(valueInDollar) {
+    // Convertimos el valor a dólares
+    let valueInYen = valueInDollar * oneEuroIs.JPY;
+    // Retornamos el valor en dólares
+    return valueInYen;
+}
+
+const fromYenToPound = function(valueInYen) {
+    // Convertimos el valor a dólares
+    let valueInPound = valueInYen * oneEuroIs.GBP;
+    // Retornamos el valor en dólares
+    return valueInPound;
+}
+// Exporta la función para usarla en otros archivos 
+// (similar a la palabra clave "export" cuando se usa webpack)
+module.exports = { sum, fromEuroToDollar, fromDollarToYen, fromYenToPound };
+
+
+
+
+
